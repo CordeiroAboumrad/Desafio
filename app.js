f56dc1aa8a82ade196e99e0b1c501de1ce933ace
jean.aboumrad <cordeiro.aboumrad@gmail.com>
2020-07-05 19:56:36 -0300

    Mudança na conexão do backend

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    // origin: 'http://localhost:8080',
+    origin: process.env.PORT,
   })
 );
 
