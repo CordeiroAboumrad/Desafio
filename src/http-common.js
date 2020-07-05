453c88521bc065e9671de902430295f82c4db14d
jean.aboumrad <cordeiro.aboumrad@gmail.com>
2020-07-05 17:04:25 -0300

    Adição de mudanças em controller e na comunicação front-back

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  // baseURL: 'http://localhost:8081/',
+  baseURL: `http://localhost:${process.env.PORT}/`,
   headers: {
     'Content-type': 'application/json',
   },
