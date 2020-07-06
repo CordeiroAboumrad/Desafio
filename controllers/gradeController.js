a039193f32c1917e302923d93ef33325a0530a8e
jean.aboumrad <cordeiro.aboumrad@gmail.com>
2020-07-05 22:00:49 -0300

    Mudança na busca por usuário

--- controllers/gradeController.js
+++ controllers/gradeController.js
@@ -44,11 +44,11 @@ const findAll = async (req, res) => {
 
 
 const findOne = async (req, res) => {
-  const id = req.params.id; //
+  const name = req.params.name; //
 
   try {
 
-    const student = await studentModel.find({"_id": id});
+    const student = await studentModel.find({"name": name});
 
     res.status(200).send(student);
 
